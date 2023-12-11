5a49d25df34e116b74391703454643caa9c36a05
naoufal-jrh <naougaljrh2003@gmail.com>
2023-12-11 15:59:34 +0100

    testing commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1 className='bg-red-700'>Testing</h1>
+      <h1 className="bg-red-700">Testing naoufal commits</h1>
     </div>
   );
 }
