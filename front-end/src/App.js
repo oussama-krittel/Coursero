d819100e7414911cffc21627a476d0500d7c5e97
oussama-krittel <krittel.oussama02@gmail.com>
2023-12-11 16:34:23 +0100

    front end updates 1

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,22 @@
-import "./App.css";
+import './App.css';
 
 function App() {
+  // const opts = {
+  //   height: '390',
+  //   width: '640',
+  //   playerVars: {
+  //     autoplay: 0,
+  //   },
+  // };
   return (
     <div>
-      <h1 className="bg-red-700">Testing naoufal commits</h1>
+      <h1 className='bg-red-700'>Testing</h1>
     </div>
   );
 }
 
 export default App;
+
+{/* <div>
+      <YouTube videoId={"k5tICunelSU"} opts={opts} />
+    </div> */}
