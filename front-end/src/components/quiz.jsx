69ec5fc5d5d98a8c36903ce890eed75eca83148b
naoufal-jrh <naougaljrh2003@gmail.com>
2023-12-16 23:38:46 +0100

    quick fixes and the accomplished quiz page is added

--- front-end/src/components/quiz.jsx
+++ front-end/src/components/quiz.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import Question from "./question";
 import Button from "./button";
 
-const Quiz = ({ acomplished, question_list }) => {
+const Quiz = ({ acomplished, question_list, score }) => {
   return (
-    <form>
-      {question_list.map((question) => (
-        <Question key={question.question_id} questObj={question} />
-      ))}
-      <Button text="Submit" submit />
-    </form>
+    <div>
+      {acomplished ? (
+        <div className="bg-blue-100 p-3 rounded text-center text-[30px] uppercase text-gray-600 font-semibold">
+          <h1 className="mb-6">you have complited this quiz !!</h1>
+          <h2>
+            your score is :{" "}
+            <span className="text-green-400 font-bold">{score}/100</span>
+          </h2>
+        </div>
+      ) : (
+        <form>
+          {question_list.map((question) => (
+            <Question key={question.question_id} questObj={question} />
+          ))}
+          <Button text="Submit" submit />
+        </form>
+      )}
+    </div>
   );
 };
 
--- front-end/src/components/quizSection.jsx
+++ front-end/src/components/quizSection.jsx
@@ -4,13 +4,18 @@ import quiz from "../assets/quiz.json";
 import { useParams } from "react-router-dom";
 const QuizSection = () => {
   const { quiz_id } = useParams();
-  const { quiz_num, acomplished, question_list } = quiz;
+  const { quiz_num, acomplished, question_list, score } = quiz;
+
   return (
     <main className="w-full p-6 flex gap-4 flex-col max-h-full hideScroll overflow-y-scroll">
       <h1 className="text-[40px] font-bold">
         Quiz <span>{quiz_num}</span>
       </h1>
-      <Quiz acomplished={acomplished} question_list={question_list} />
+      <Quiz
+        acomplished={acomplished}
+        question_list={question_list}
+        score={score}
+      />
     </main>
   );
 };
